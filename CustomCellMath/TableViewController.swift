5982787f88ae367e73fbe7f574f3610973e149b8
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MBP.fios-router.home>
2016-10-09 10:13:18 -0400

    Done

--- CustomCellMath/TableViewController.swift
+++ CustomCellMath/TableViewController.swift
@@ -19,13 +19,48 @@ class TableViewController: UITableViewController {
         generateData()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numbers.count
+    }
     
-    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mathCell", for: indexPath) as! MathTableViewCell
+        
+//        var arrayIndex = indexPath.row
+//        var selectedArray = self.numbers[arrayIndex]
+//        cell.firstNumberLabel.text = String(selectedArray[0])
+//        cell.secondNumberLabel.text = String(selectedArray[1])
+//        cell.thirdNumberLabel.text = String(selectedArray[2])
+//        cell.fourthNumberLabel.text = String(selectedArray[3])
+        
+        cell.firstNumberLabel.text = String(numbers[indexPath.row][0])
+        cell.secondNumberLabel.text = String(numbers[indexPath.row][1])
+        cell.thirdNumberLabel.text = String(numbers[indexPath.row][2])
+        cell.fourthNumberLabel.text = String(numbers[indexPath.row][3])
+        
+        
+        return cell
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMath" {
+            let destinationViewController = segue.destination as! DisplayMathViewController
+            
+            
+            let selectedMath = self.tableView.indexPathForSelectedRow?.row
+            
+            if let unwrappedRow = selectedMath {
+                destinationViewController.numbers = self.numbers[unwrappedRow]
+            }
+
+        }
+    }
 
 }
-
 
 
 // MARK: - Generating an array of Data
@@ -47,6 +82,7 @@ extension TableViewController {
     func randomNumberFromOneTo(_ number: Int) -> Int {
         return Int(arc4random_uniform(UInt32(number)) + 1)
     }
+    
     
     
 }
